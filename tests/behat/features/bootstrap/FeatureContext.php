270adab7cc9705ce0f8831645ad0c85cda9e539b
Miranda Mafe <miranda.mafe@gmail.com>
2019-09-18 14:14:25 -0400

    Clean up behat failure screenshots

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -4,6 +4,8 @@ use Behat\Behat\Tester\Exception\PendingException;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
+
+
 use Behat\Gherkin\Node\TableNode;
 use Behat\Behat\Hook\Scope\AfterStepScope;
 use Behat\Mink\Driver\Selenium2Driver;
@@ -30,9 +32,7 @@ use Behat\MinkExtension\Context\RawMinkContext;
    */
   public function printLastResponseOnError(AfterStepScope $event)
   {
-    echo "-------------FEATURECONTEXT: printLastResponseOnError!-------";
       if (!$event->getTestResult()->isPassed()) {
-        echo "-------------FEATURECONTEXT: printLastResponseOnError! DIDN'T PASS-------";
           $this->saveDebugScreenshot();
       }
   }
@@ -42,34 +42,8 @@ use Behat\MinkExtension\Context\RawMinkContext;
    */
   public function saveDebugScreenshot()
   {
-    echo "-------------FEATURECONTEXT: saveDebugScreenshot! -------";
-    $driver = $this->getSession()->getDriver();
-
-    // if (!$driver instanceof Selenium2Driver) {
-    //   echo "-------------FEATURECONTEXT: driver instanceof Selenium2Driver! -------";
-    //     return;
-    // }
-
-    // if (!getenv('BEHAT_SCREENSHOTS')) {
-    //   echo "-------------FEATURECONTEXT: NOT BEHAT_SCREENSHOTS! -------";
-    //     return;
-    // }
-
-    echo "-------------FEATURECONTEXT: GETTING FILENAME AND PATH ! -------";
     $filename = microtime(true).'.png';
-    // $path = $this->getContainer()
-    //     ->getParameter('kernel.root_dir').'/../behat_screenshots';
-
-    // if (!file_exists($path)) {
-    //     mkdir($path);
-    // }
-
-    echo "-------------FEATURECONTEXT: SAVING SCREENSHOTS! -------";
-    // echo("----- saving screenshot" . $filename . "to" . $path);
-    echo("----- saving screenshot" . $filename);
-
-    // $this->saveScreenshot($filename, $path);
-
-    $this->saveScreenshot($filename, 'var/behat_screenshots');
+    $path = 'var/behat_screenshots';
+    $this->saveScreenshot($filename, $path);
   }
 }
