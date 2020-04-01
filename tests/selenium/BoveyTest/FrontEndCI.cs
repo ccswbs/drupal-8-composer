4271d26538cd605bfd00535154430fbdf0299efc
Miranda Mafe <miranda.mafe@gmail.com>
2020-04-01 13:29:13 -0400

    Log in to confirm build hook only visible for publishers and admins

--- tests/selenium/BoveyTest/FrontEndCI.cs
+++ tests/selenium/BoveyTest/FrontEndCI.cs
@@ -20,6 +20,8 @@ namespace BoveyTest
     {
         private string _adminUser;
         private string _adminPass;
+        private string _frontEndEnv = "preview_changes";
+        private string _frontEndEnvTitle = "Preview Changes environment deployment";
 
         [TestInitialize]
         [DeploymentItem("appsettings*.json")]
@@ -42,10 +44,9 @@ namespace BoveyTest
         }
 
         [TestMethod]
-        public void BuildTriggerSuccessful()
+        public void BuildHookSuccessful()
         {
-            var frontEndEnv = "preview_changes";
-            var buildTriggerSubmitBtn = "edit-submit";
+            var buildHookSubmitBtn = "edit-submit";
             string[] permittedRoles = new string[] {"publisher"};
 
             TurnOnLDAPMixedMode();
@@ -56,15 +57,15 @@ namespace BoveyTest
                 DrupalLogout();
                 DrupalLogin(testUser.Name, testUser.Password);
 
-                // Go to Build Trigger deployment page
-                DrupalGet("/admin/build_hooks/deployments/" + frontEndEnv);
+                // Go to Build Hook deployment page
+                DrupalGet("/admin/build_hooks/deployments/" + _frontEndEnv);
 
-                // Activate build trigger
-                Click(buildTriggerSubmitBtn);
+                // Activate build hook
+                Click(buildHookSubmitBtn);
 
                 // Check if successful message appears after testing connection
-                var successfulBuildTriggerMessage = Driver.FindElementsByXPath($"//div[contains(@class, 'messages--status') and text()[contains(.,'Deployment triggered for environment')]]");
-                Assert.AreEqual(successfulBuildTriggerMessage.Count, 1);
+                var successfulBuildHookMessage = Driver.FindElementsByXPath($"//div[contains(@class, 'messages--status') and text()[contains(.,'Deployment triggered for environment')]]");
+                Assert.AreEqual(successfulBuildHookMessage.Count, 1);
 
                 // Delete test user
                 DrupalLogout();
@@ -76,48 +77,114 @@ namespace BoveyTest
         }
 
         [TestMethod]
-        public void BuildTriggerOnlyVisibleForPublisherAndAdmin()
+        public void BuildHookOnlyVisibleForPublisherAndAdmin()
         {
-            var permissionIDStub = "trigger-deployments";
-            string[] permittedRoles = new string[] {"administrator", "publisher"};
+            string[] permittedRoles = new string[] {"administrator","publisher"};
+
+            // Set up
+            TurnOnLDAPMixedMode();
+            DrupalUser testUser = CreateUser(permittedRoles);
+            DrupalLogout();
+            DrupalLogin(testUser.Name, testUser.Password);
+
+            // Confirm that build hook is visible for permitted roles
+            DrupalGet("/admin/build_hooks/deployments/" + _frontEndEnv);
+            Assert.AreEqual(CheckIfPageTitleIsCorrect(_frontEndEnvTitle),true);
             
-            // Go to Permissions page
-            DrupalGet("/admin/people/permissions");
+            // Update user to have all roles except permitted roles
+            DrupalLogout();
+            DrupalLogin(_adminUser,_adminPass);
+            AddRoles(testUser);
+            RemoveRoles(testUser, permittedRoles);
+            DrupalLogout();
+            DrupalLogin(testUser.Name, testUser.Password);
 
-            // Check if permitted roles have permission
-            foreach (string role in permittedRoles){
-                var adminPermission = Driver.FindElementsByXPath($"//input[contains(@id, 'edit-{role}-{permissionIDStub}') and contains(@checked, 'checked')]");
-                Assert.AreEqual(adminPermission.Count, 1);
-            }
+            // Confirm that build hook is not visible for permitted roles
+            DrupalGet("/admin/build_hooks/deployments/" + _frontEndEnv);
+            Assert.AreEqual(CheckIfPageTitleIsCorrect("Access denied"),true);
 
-            // Check that no other roles have permission
-            var numberOfRolesWithPermission = Driver.FindElementsByXPath($"//input[contains(@id, '-{permissionIDStub}') and contains(@checked, 'checked')]");
-            Assert.AreEqual(numberOfRolesWithPermission.Count, permittedRoles.Length);
+            // Clean up
+            DrupalLogout();
+            DrupalLogin(_adminUser,_adminPass);
+            DeleteUser(testUser.Name, true);
+            TurnOffLDAPMixedMode();
         }
 
-        public void DeleteUser (string name, bool deleteContent = false){
+        void DeleteUser (string name, bool deleteContent = false){
             // Go to User Edit page
+            GoToEditUserPage(name);
+            
+            // Confirm it is the correct user
+            if(CheckIfPageTitleIsCorrect(name) == true){
+                Click("edit-delete");
+
+                // adjust delete content settings
+                if(deleteContent == true){
+                    Click("edit-user-cancel-method-user-cancel-delete");
+                }else{
+                    Click("edit-user-cancel-method-user-cancel-reassign");
+                }
+
+                Click("edit-submit");
+            }
+        }
+        
+        // If no roles are provided, by default, method removes all available roles
+        void RemoveRoles(DrupalUser user, string[] roles = null){
+            GoToEditUserPage(user.Name);
+
+            if(roles == null){
+                // Remove all roles
+                var checkboxes = Driver.FindElementsByXPath($"//input[contains(@id,'edit-roles-') and contains(@checked,'checked')]");
+                foreach(var checkbox in checkboxes){
+                    ScrollAndClick(checkbox);
+                }
+            }else{
+                // Remove specific roles
+                foreach (var role in roles)
+                {
+                    var checkbox = Driver.FindElementByXPath($"//input[@id='edit-roles-{role}' and contains(@checked,'checked')]");
+                    ScrollAndClick(checkbox);
+                }
+            }
+            Click("edit-submit");
+        }
+
+        // If no roles are provided, by default, method adds all available roles
+        void AddRoles(DrupalUser user, string[] roles = null){
+            GoToEditUserPage(user.Name);
+            if(roles == null){
+                // Add all roles
+                var checkboxes = Driver.FindElementsByXPath($"//input[contains(@id,'edit-roles-') and not(contains(@checked,'checked'))]");
+                foreach(var checkbox in checkboxes){
+                    ScrollAndClick(checkbox);
+                }
+            }else{
+                // Add specific roles
+                foreach (var role in roles)
+                {
+                    var checkbox = Driver.FindElementByXPath($"//input[@id='edit-roles-{role}' and not(contains(@checked,'checked'))]");
+                    ScrollAndClick(checkbox);
+                }
+            }
+            Click("edit-submit");
+        }
+
+        void GoToEditUserPage(string name){
             DrupalGet("/admin/people");
             Driver.FindElementByXPath($"//a[@content='{name}']").Click();
             Driver.FindElementByXPath($"//a[text()='Edit']").Click();
-
-            // Confirm it is the correct user
-            var userToDelete = Driver.FindElementsByXPath($"//h1[text()='{name}']");
-            if(userToDelete != null){
-                Click("edit-delete");
-            }
-
-            // adjust delete content settings
-            if(deleteContent == true){
-                Click("edit-user-cancel-method-user-cancel-delete");
-            }else{
-                Click("edit-user-cancel-method-user-cancel-reassign");
-            }
-
-            Click("edit-submit");
         }
-        
-        public DrupalUser CreateUser(string[] roles, bool blocked = false)
+
+        bool CheckIfPageTitleIsCorrect(string title){
+            var heading = Driver.FindElementsByXPath($"//h1[contains(@class,'page-title') and (contains(text(),'{title}'))]");
+            if (heading.Count() == 1){
+                return true;
+            }
+            return false;
+        }
+
+        DrupalUser CreateUser(string[] roles = null, bool blocked = false)
         {
             var name = "selenium-user-" + RandomName();
             var mail = name + "@example.com";
@@ -133,10 +200,13 @@ namespace BoveyTest
             {
                 Click("edit-status-0");
             }
-            foreach (var role in roles)
-            {
-                var checkbox = Driver.FindElementByXPath($"//label[@for='edit-roles-{role}']");
-                ScrollAndClick(checkbox);
+
+            if (roles != null){
+                foreach (var role in roles)
+                {
+                    var checkbox = Driver.FindElementByXPath($"//label[@for='edit-roles-{role}']");
+                    ScrollAndClick(checkbox);
+                }
             }
             Click("edit-submit");
             return new DrupalUser { Name = name, Password = pass };
